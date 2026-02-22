d791fa99bd163adb50780bc0565fff687f250ac2
Golfring <golfring.dove@hotmail.com>
2026-02-22 14:47:51 +0700

    start Portfolio react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Golfring Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
