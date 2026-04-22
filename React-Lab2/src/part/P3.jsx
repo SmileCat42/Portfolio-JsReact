45f95a17c4f0893a53247510bf365f23e25c5ed2
Golfring <golfring.dove@hotmail.com>
2026-04-22 21:42:04 +0700

    try navbar

--- React-Lab2/src/part/P3.jsx
+++ React-Lab2/src/part/P3.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const P3 = () => {
   return (
-    <div>ppppppppppppppp 3</div>
+    <div className="flex mx-auto mt-4 w-[800px] bg-linear-to-br from-gray-700 to-black
+    rounded-2xl py-4">
+      <div className="flex cols-2 justify-center">
+        <span className="text-white font-bold pl-10
+        hover:scale-110 transition-all duration-300">
+            Portfolio<span className="text-yellow-500 font-bold">.</span>
+        </span>
+        <span className="text-yellow-400 pl-15
+        hover:scale-110 transition-all duration-300">
+            Home
+        </span>
+        <span className="text-yellow-400 pl-10
+        hover:scale-110 transition-all duration-300">
+            About
+        </span>
+        <span className="text-yellow-400 pl-10
+        hover:scale-110 transition-all duration-300 pr-15">
+            Product
+        </span>
+        <a href="/"
+        className="hover:scale-110 transition-all duration-300">
+            <span className="bg-linear-to-br from-yellow-400 to-yellow-600 py-2 px-3 font-semibold 
+            text-white rounded-2xl ">Sign in</span>
+        </a>
+      </div>
+    </div>
   )
 }
 export default P3
